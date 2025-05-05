87b15e32556dc6669097708b39069dcbfee5acc1
Laman09 <ljalilli09@gmail.com>
2025-05-06 01:32:25 +0400

    commit

--- park-cinema/src/main.jsx
+++ park-cinema/src/main.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
 import FAQ from './components/FAQ/FAQ'
+import SignIn from './components/SignIn/SignIn'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path = '/' element = {<Layout />}>
-          <Route index element = {<Home />}></Route>
+        <Route path = '/az' element = {<Layout />}>
+          <Route index path = '/az' element = {<Home />}></Route>
           <Route path = '/az/faq' element = {<FAQ />}></Route>
+          <Route path = '/az/sign-in' element = {<SignIn />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
